44dd4bc712c25d5bca0e2692c1f7359f693c0a49
DevloperParth <pachoudhary@bestpeers.com>
2021-10-11 21:08:25 +0530

    changeproduction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mail.com',
+    port: 587,
+    domain: 'heroku.com',
+    user_name: 'parthchoudhary270@gmail.com',
+    password: '9399541891',
+    authentication: 'plain',
+    enable_starttls_auto: true }
 end
