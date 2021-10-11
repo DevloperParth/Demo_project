8f7cb2dea7caf72aa0b53b8bdb015568edbccc8f
DevloperParth <pachoudhary@bestpeers.com>
2021-10-11 21:01:14 +0530

    fixmailagain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
 
+
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
@@ -75,9 +76,8 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  host = '<parthdemoproject>.herokuapp.com'
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: host}
+  config.action_mailer.default_url_options = { host: 'parthdemoproject.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
@@ -87,7 +87,7 @@ Rails.application.configure do
     enable_starttls_auto: true,
     user_name: "parthchoudhary270@gmail.com",
     password: "9399541891", 
-    domain: "heroku.com",
+    domain: "smtp.gmail.com",
     openssl_verify_mode: "none",
   }
 end
