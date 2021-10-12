b431d5ef1e17e776003f15d3b39192cc3ae92c6d
DevloperParth <pachoudhary@bestpeers.com>
2021-10-12 10:12:04 +0530

    fix registarionform issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,15 +117,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'parthdemoproject.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.mail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: 'parthchoudhary270@gmail.com',
-    password: '9399541891',
-    authentication: 'plain',
-    enable_starttls_auto: true }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { :host => 'parthdemoproject.herokuapp.com' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.mail.com',
+  #   port: 587,
+  #   domain: 'gmail;.com',
+  #   user_name: 'parthchoudhary270@gmail.com',
+  #   password: '9399541891',
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true }
 end
