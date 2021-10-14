a7e25179043cf3ce3088050c3fc00dfc794d6b4c
DevloperParth <pachoudhary@bestpeers.com>
2021-10-14 16:22:54 +0530

    app passwod change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,7 +129,7 @@ Rails.application.configure do
     port: 587,
     domain: 'parthdemoproject.herokuapp.com',
     user_name: 'parthchoudhary270@gmail.com',
-    password: 'jnxwhhgcbpiopagr',
+    password: '9399541891',
     authentication: 'plain',
     enable_starttls_auto: true }
 end
